a954c7856d167788edfd5b1e26650122e49b3ded
Seth Fitzsimmons <seth@mojodna.net>
2018-07-11 11:59:39 -0700

    Use dynamic ormconfig for DATABASE_URL support

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  synchronize: true,
+  logging: true,
+  entities: ["src/entity/**/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  subscribers: ["src/subscriber/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber"
+  }
+};
